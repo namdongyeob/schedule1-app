7570c229a1bb4cfa15e9d6e7d300e7100329b1da
namdongyeob <ehdduq1324@gmail.com>
2026-04-22 09:59:38 +0900

    feat: Cookie/Session 로그인 및 Filter 인증 구현

--- src/main/java/com/example/schedule1app/user/repository/UserRepository.java
+++ src/main/java/com/example/schedule1app/user/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.schedule1app.user.repository;
 import com.example.schedule1app.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    // 이메일로 유저를 찾는 쿼리 메서드 추가
+    Optional<User> findByEmail(String email);
 }
